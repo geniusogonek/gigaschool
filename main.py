8f05a24b034de8ce8444358537ad866d5264dbaf
geniusogonek <ahmtimur30@gmail.com>
2025-11-27 13:25:24 +0500

    xl export to db

--- main.py
+++ main.py
@@ -5,34 +5,60 @@ import speech_recognition as sr
 
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
-
-from gigachat import GigaChat
-from gigachat.models import Chat, Messages, MessagesRole
+from aiogram.filters import BaseFilter
 
 from pydub import AudioSegment
 
 from dotenv import load_dotenv
 
 from db.core import init_db, get_session_maker
+from db.database import import_schedule_from_json, get_user_grade
+from parse_files.parse_excel import parse_schedule_excel
+from gigachatapi import get_answer
+
+
+class DocumentTypeFilter(BaseFilter):
+    def __init__(self, extensions: list[str]):
+        self.extensions = [ext.lower() for ext in extensions]
+
+    async def __call__(self, message: Message) -> bool:
+        if not message.document or not message.document.file_name:
+            return False
+        file_ext = message.document.file_name.split('.')[-1].lower()
+        return file_ext in self.extensions
+
 
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-API_KEY = os.getenv("API_KEY")
-POSTGRES_URL = os.getenv("POSTGRES_SQL")
+POSTGRES_URL = os.getenv("POSTGRES_URL")
 
 logging.basicConfig(level=logging.INFO)
 bot = Bot(BOT_TOKEN)
 dp = Dispatcher()
 
-payload = Chat(
-    messages=[
-        Messages(
-            role=MessagesRole.SYSTEM,
-            content="Отвечай без форматирования markdown, только текст, без форматирования"
-        )
-    ],
-)
+
+engine = None
+SessionMaker = None
+
+
+@dp.message(DocumentTypeFilter(["xlsx", "xls"]))
+async def get_document(message: Message):
+    file = await bot.get_file(message.document.file_id)
+    ext = message.document.file_name.split(".")[-1]
+    filename = f"{message.from_user.id}.{ext}"
+    await bot.download_file(file.file_path, filename)
+
+
+
+    async with SessionMaker() as session:
+        grade = await get_user_grade(session, message.from_user.id)
+        data = parse_schedule_excel(filename, grade)
+        print(data)
+        await import_schedule_from_json(session, tg_id=message.from_user.id, schedule_data=data)
+
+    if os.path.exists(filename):
+        os.remove(filename)
 
 
 @dp.message()
@@ -58,16 +84,15 @@ async def start(message: Message):
     else:
         text = message.text
 
-    print(message.from_user.username, ": ", text)
-
-    with GigaChat(credentials=API_KEY, verify_ssl_certs=False) as giga:
-        payload.messages.append(Messages(role=MessagesRole.USER, content=text))
-        response = giga.chat(payload)
-        payload.messages.append(response.choices[0].message)
-        await message.answer(response.choices[0].message.content)
+    logging.info(f"{message.from_user.username}, {message.from_user.id}: {text}")
+    await message.answer(get_answer(text))
 
 
 async def main():
+    global engine, SessionMaker
+    engine = await init_db(POSTGRES_URL)
+    SessionMaker = get_session_maker(engine)
+
     await dp.start_polling(bot)
 
 
