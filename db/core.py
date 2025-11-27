105cd73732941e32c8ff62a9670576958de6c1c2
geniusogonek <ahmtimur30@gmail.com>
2025-11-27 10:17:29 +0500

    added database structure

---
+++ db/core.py
@@ -0,0 +1,29 @@
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+async def init_db(url: str):
+    """
+    url пример:
+    postgresql+asyncpg://user:password@localhost:5432/mydb
+    """
+
+    engine = create_async_engine(url, echo=False)
+
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    return engine
+
+
+def get_session_maker(engine):
+    return sessionmaker(
+        engine,
+        expire_on_commit=False,
+        class_=AsyncSession
+    )
---
+++ db/models.py
@@ -0,0 +1,75 @@
+from sqlalchemy import (
+    Column,
+    Integer,
+    BigInteger,
+    String,
+    Date,
+    Text,
+    ForeignKey,
+    UniqueConstraint,
+)
+from sqlalchemy.orm import relationship
+from db.core import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True)
+    tg_id = Column(BigInteger, unique=True, nullable=False)
+
+    grade = Column(String)
+    subjects = relationship("Subject", back_populates="user", cascade="all, delete")
+    schedule = relationship("Schedule", back_populates="user", cascade="all, delete")
+
+
+class Subject(Base):
+    __tablename__ = "subjects"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+
+    name = Column(String(255), nullable=False)
+    classroom = Column(String(50))
+
+    user = relationship("User", back_populates="subjects")
+    lessons = relationship("Schedule", back_populates="subject")
+
+    __table_args__ = (
+        UniqueConstraint("user_id", "name", name="uq_user_subject_name"),
+    )
+
+
+class Schedule(Base):
+    __tablename__ = "schedule"
+
+    id = Column(Integer, primary_key=True)
+
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    date = Column(Date, nullable=False)
+    lesson_number = Column(Integer, nullable=False)
+    subject_id = Column(Integer, ForeignKey("subjects.id", ondelete="SET NULL"))
+
+    user = relationship("User", back_populates="schedule")
+    subject = relationship("Subject", back_populates="lessons")
+    homework = relationship("Homework", back_populates="schedule", cascade="all, delete")
+
+    __table_args__ = (
+        UniqueConstraint("user_id", "date", "lesson_number",
+                         name="uq_user_day_lesson"),
+    )
+
+
+class Homework(Base):
+    __tablename__ = "homework"
+
+    id = Column(Integer, primary_key=True)
+    schedule_id = Column(
+        Integer,
+        ForeignKey("schedule.id", ondelete="CASCADE"),
+        nullable=False
+    )
+
+    text = Column(Text, nullable=True)
+
+    schedule = relationship("Schedule", back_populates="homework")
